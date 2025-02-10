84dcbc3ffa731d7e28d07a25bb812635fffe9e0d
Greyson Knippel <112649396+gaknippel@users.noreply.github.com>
2025-02-09 16:37:08 -0800

    Updated both parts vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,15 +1,47 @@
 #include <vector>
 #include <iostream>
-
 using namespace std;
 
 
-void part1() {
-    //TODO: Implement part 1
+void part1() 
+{
+    vector <int> vec1;
+
+    for(int i = 0; i <50000; i++)
+    {
+        vec1.push_back(i);
+    }
+
+    cout << "After adding elements: Size = " << vec1.size() << ", Capacity = " << vec1.capacity() << endl;
+
+    vec1.resize(vec1.size()/2);
+
+    cout << "After resizing: Size = " << vec1.size() << ", Capacity = " << vec1.capacity() << endl;
+
+    vec1.shrink_to_fit();
+
+    cout << "After shirnk_to_fit(): Size = " << vec1.size() << ", Capacity = " << vec1.capacity() << endl;
 }
 
-void part2() {
-    //TODO: Implement part 2
+void part2() 
+{
+    vector <bool> boolVec;
+    vector <char> charVec;
+
+    for(int i = 0; i < 50000; i++)
+    {
+        boolVec.push_back(i);
+        charVec.push_back(i);
+    }
+
+    int byteSizeBool = boolVec.size();
+    int byteSizeChar = charVec.size();
+
+    cout << "Memory usage of std::vector<bool>: " << byteSizeBool << " bytes" << endl;
+
+    cout << "Memory usage of std::vector<bool>: " << byteSizeChar << " bytes" << endl;
+
+    
 }
 
 int main() {
